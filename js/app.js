7cd0427687669022a500e4001be88a9a3505c5e7
laugeeme <registro.laura@gmail.com>
2020-05-20 12:39:41 +0200

    create function to make object with the info we need

--- js/app.js
+++ js/app.js
@@ -4,11 +4,10 @@
 const cart = document.getElementById('carrito');
 const courses = document.getElementById('lista-cursos');
 
-
 //function to add course to cart
 function buyCourse(e) {
   e.preventDefault();
-  //delegation to add to cart
+  //use delegation to add to cart
   if (e.target.classList.contains('agregar-carrito')) {
     const course = e.target.parentElement.parentElement;
     //send the selected course to take his info
@@ -16,8 +15,15 @@ function buyCourse(e) {
   }
 }
 
+//function creates and object with the info of the course
 function readCourseInfo(course) {
-  console.log(course);
+  const courseInfo = {
+    imagen: course.querySelector('img').src,
+    title: course.querySelector('h4').textContent,
+    price: course.querySelector('.precio span').textContent,
+    id: course.querySelector('a').getAttribute('data-id'),
+  };
+  console.log(courseInfo);
 }
 
 //listeners
